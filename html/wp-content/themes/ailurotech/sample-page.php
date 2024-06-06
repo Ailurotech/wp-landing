57a9aaa8148c8284e51649a7eec499fa7ede2e69
Gavin_G <gzh9523@gmail.com>
2024-06-06 19:16:14 +0930

    submit contact files

--- html/wp-content/themes/ailurotech/sample-page.php
+++ html/wp-content/themes/ailurotech/sample-page.php
@@ -12,5 +12,11 @@ $context['post'] = Timber::get_post();
 $context['sample_text'] = get_field('sample_text');
 $context['sample_title'] = get_field('title');
 
+$contact_us_post = Timber::get_post(array(
+  'name' => 'contact-us-section-title', 
+  'post_type' => 'post',
+));
+$context['contact_section'] = $contact_us_post;
+$context['contact_bg_video']  = get_field('bgvideo',$contact_us_post);
 // Render the Twig template
 Timber::render('partial/sample-page.twig', $context);
