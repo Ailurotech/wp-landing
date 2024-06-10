e0eeb7d08e0a2dcce725966eca2d7949e14eac3f
Tracy-maker <ydlvns@gmail.com>
2024-06-10 16:33:49 +1000

    add the logo

--- html/wp-content/themes/ailurotech/functions/navbar.php
+++ html/wp-content/themes/ailurotech/functions/navbar.php
@@ -3,19 +3,22 @@
 use Timber\Timber;
 use Timber\Menu;
 
-function register_menus() {
+function register_menus()
+{
     register_nav_menus(
         array(
-            'primary' => __( 'Primary Menu', 'ailurotech' ),
+            'primary' => __('Primary Menu', 'ailurotech'),
         )
     );
 }
-add_action( 'init', 'register_menus' );
+add_action('init', 'register_menus');
 
-function get_timber_menu() {
+function get_timber_menu()
+{
     $context = Timber::context();
     $context['menu'] = new Menu('primary');
     $context['site'] = new TimberSite();
     Timber::render('navbar.twig', $context);
 }
 add_action('wp_head', 'get_timber_menu');
+
