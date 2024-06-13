b1236b7d740f0e121fe4280b3f52241cf6c72f4d
Depeng Sun <shangguming@gmail.com>
2024-06-14 05:42:03 +0930

    title and link finished

--- html/wp-content/themes/ailurotech/index.php
+++ html/wp-content/themes/ailurotech/index.php
@@ -16,6 +16,29 @@
 $context = Timber::context();
 
 $icon_url=get_field("icon");
+
+$first_row_title=get_field("first_row_title");
+$first_row_links=get_field("first_row_links");
+
+$second_row_title=get_field("second_row_title");
+$second_row_links=get_field("second_row_links");
+
+$third_row_title=get_field("third_row_title");
+$third_row_links=get_field("third_row_links");
+
+$title_of_column=get_field("title_of_column");
+
+
+$context['first_row_title']=explode("!", $first_row_title);
+$context['first_row_links']=explode("!", $first_row_links);
+$context['second_row_title']=explode("!", $second_row_title);
+$context['second_row_links']=explode("!", $second_row_links);
+$context['third_row_title']=explode("!", $third_row_title);
+$context['third_row_links']=explode("!", $third_row_links);
+
+$context['title_of_column']=explode("!", $title_of_column);
+
 $context['links_array'] = explode(" ", $icon_url);
+
 
 Timber::render('partial/index.twig', $context);
