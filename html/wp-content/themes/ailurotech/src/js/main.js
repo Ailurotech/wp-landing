3e382807ad96cd7fc934a0543918c217c9349f1a
Depeng Sun <shangguming@gmail.com>
2024-07-23 08:09:04 +0930

    carousel

--- html/wp-content/themes/ailurotech/src/js/main.js
+++ html/wp-content/themes/ailurotech/src/js/main.js
@@ -6,3 +6,12 @@ document.addEventListener('DOMContentLoaded', function () {
       dropdownMenu.classList.toggle('active');
   });
 });
+//carousel section
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true
+  
+});
